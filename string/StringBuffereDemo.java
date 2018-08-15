d1b592029da6a7db5ed5ce545fee76884a08ce18
kar6000 <34102279+kar6000@users.noreply.github.com>
2018-08-14 21:49:39 -0700

    Add files via upload

---
+++ string/StringBuffereDemo.java
@@ -0,0 +1,16 @@
+package july24;
+
+public class StringBuffereDemo {
+
+	public static void main(String[] args) {
+		
+		
+		
+		StringBuffer sb = new StringBuffer("California");
+		//sb.reverse();		
+		System.out.println(sb.reverse());
+		
+		
+	}
+
+}
